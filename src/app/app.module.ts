6a5e79505194c7a0826d7634a35432709c14b5d7
OleksandrYatsiuk <oleksandr.yatsiuk@gmail.com>
2020-01-18 16:36:38 +0200

    new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,18 @@ import { HttpClientModule } from '@angular/common/http';
 import { CreatePizzaComponent } from './create-pizza/create-pizza.component';
 import { FormsModule } from '@angular/forms';
 import { PizzaFilterPipe } from './shared/pizza-filter.pipe';
-import { HeaderComponent } from './main/header/header.component'
+import { HeaderComponent } from './shared/header/header.component';
+import { ContentComponent } from './main/content/content.component';
+import { PizzaItemComponent } from './main/content/pizza-item/pizza-item.component';
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
     CreatePizzaComponent,
     PizzaFilterPipe,
-    HeaderComponent
+    HeaderComponent,
+    PizzaItemComponent,
+    ContentComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/shared/pizza-filter.pipe.ts
+++ src/app/shared/pizza-filter.pipe.ts
@@ -1,8 +1,6 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { Root } from './root.service';
 
-
-
 @Pipe({
     name: "pizzaFilter"
 })
