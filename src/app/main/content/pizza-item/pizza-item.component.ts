0c5a8ede438f8f2dbc88969355caf7ee50607949
Anatoliy Movchan <anatoliy.movchan@yahoo.com>
2020-01-19 11:10:42 +0200

    basket event

--- src/app/main/content/pizza-item/pizza-item.component.ts
+++ src/app/main/content/pizza-item/pizza-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { RootService } from '../../../shared/root.service';
 
 
@@ -9,4 +9,9 @@ import { RootService } from '../../../shared/root.service';
 })
 export class PizzaItemComponent {
   @Input() item;
+  constructor(private rootService: RootService) {}
+  onClickMe(item) {
+    this.rootService.$bashChanges.next(item);
+     localStorage.setItem(item.id, JSON.stringify(item));
+  }
 }
--- src/app/shared/root.service.ts
+++ src/app/shared/root.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 @Injectable({ providedIn: 'root' })
 export class RootService {
+  $bashChanges = new Subject<any>();
 
-    constructor(private http: HttpClient) {
+  constructor(private http: HttpClient) {
     }
     fetchItems(): Observable<any[]> {
         return this.http.get<any[]>('http://localhost:3000/pizzas');
