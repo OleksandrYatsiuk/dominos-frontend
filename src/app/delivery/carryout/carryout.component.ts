e10b02cfff314e037ff2d4ce717ce936aff9cd03
yatsiuk-oa <yatsiuk.oa@gbksoft.com>
2020-02-05 14:19:56 +0200

    carryout form

--- src/app/delivery/carryout/carryout.component.ts
+++ src/app/delivery/carryout/carryout.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, Validators, FormBuilder, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-carryout',
@@ -6,10 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./carryout.component.scss']
 })
 export class CarryoutComponent implements OnInit {
-
-  constructor() { }
+  carryOut: FormGroup;
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.carryOut = this.formBuilder.group({
+      firstName: ["", [Validators.required, Validators.maxLength(15)]],
+      phone: ["", [Validators.required, Validators.pattern("[0-9]{10}")]],
+      email: ["", [Validators.required, Validators.email]],
+      restaurant: this.formBuilder.group({
+        restaurant: ["", [Validators.required]],
+        comment: ["", []],
+      }),
+      date: this.formBuilder.group({
+        date: ["", [Validators.required]],
+        time: ["", [Validators.required]],
+      }),
+      payment: this.formBuilder.group({
+        coupon: ["", []],
+        remainder: ["", []],
+        type: ["", [Validators.required]],
+      })
+    })
   }
 
 }
