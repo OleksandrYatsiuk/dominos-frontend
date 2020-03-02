949a97190adaf3eccb67cb3ea44aeee7d203ce51
OleksandrYatsiuk <oleksandr.yatsiuk@gmail.com>
2020-03-02 23:11:42 +0200

    registration form

---
+++ src/app/auth/registration/registration.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { RootService } from 'src/app/shared/root.service';
+
+@Component({
+  selector: 'app-registration',
+  templateUrl: './registration.component.html',
+  styleUrls: ['./registration.component.scss']
+})
+export class RegistrationComponent implements OnInit {
+
+  registerForm: FormGroup;
+  constructor(
+    private rootService: RootService,
+    private formBuilder: FormBuilder
+  ) { }
+
+  ngOnInit() {
+
+    this.registerForm = this.formBuilder.group({
+
+      fullName: ['', [Validators.required, Validators.maxLength(15)]],
+      username: ['', [Validators.required, Validators.maxLength(15)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]],
+      confirmPassword: ['', [Validators.required]]
+
+    });
+  }
+  register() {
+    console.log(this.registerForm.value);
+  }
+
+
+
+}
