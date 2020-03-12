51901397dbc1539b911fb2add762514fb8e5039c
OleksandrYatsiuk <oleksandr.yatsiuk@gmail.com>
2020-03-12 07:28:21 +0200

    improved structure order

--- src/app/products/pizza/pizza-overview/pizza-overview.component.ts
+++ src/app/products/pizza/pizza-overview/pizza-overview.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { RootService } from '../../../shared/root.service';
 import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { pluck } from 'rxjs/operators';
 
 
 @Component({
@@ -13,6 +14,9 @@ import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 export class PizzaOverviewComponent implements OnInit {
   pizza: any;
   pizzaForm: FormGroup;
+  categories = [{ value: "Краща Ціна" }, { value: "Класичні" }, { value: "Фірмові" }]
+  ingredients;
+  url: string | ArrayBuffer = '../../assets/data/pizzas/default.jpg';
 
   constructor(private route: ActivatedRoute,
     private rootService: RootService,
@@ -20,28 +24,33 @@ export class PizzaOverviewComponent implements OnInit {
   ) {
 
     this.pizza = this.route.snapshot.data.pizza;
+    this.url = this.pizza.image;
   }
 
 
   ngOnInit() {
 
 
-
     this.pizzaForm = this.formBuilder.group({
-      name: ['', [Validators.required, Validators.maxLength(15)]],
-      category: ['', [Validators.required]],
-      ingredients: [[], [Validators.required]],
-      image: [''],
+      name: [this.pizza.name, [Validators.required, Validators.maxLength(15)]],
+      category: [this.pizza.category, [Validators.required]],
+      ingredients: [this.pizza.ingredients, [Validators.required]],
+      image: [this.pizza.image],
       weight: this.formBuilder.group({
-        small: ['', [Validators.required]],
-        middle: ['', [Validators.required]],
-        big: ['', [Validators.required]],
+        small: [this.pizza.weight.small, [Validators.required]],
+        middle: [this.pizza.weight.middle, [Validators.required]],
+        big: [this.pizza.weight.big, [Validators.required]],
       }),
       price: this.formBuilder.group({
-        low: ['', [Validators.required]],
-        medium: ['', [Validators.required]],
-        high: ['', [Validators.required]],
+        low: [this.pizza.price.low, [Validators.required]],
+        medium: [this.pizza.price.medium, [Validators.required]],
+        high: [this.pizza.price.high, [Validators.required]],
       }),
+
+    });
+
+    this.rootService.getIngredientsList().subscribe(res => {
+      this.ingredients = res.result;
     });
   }
 
