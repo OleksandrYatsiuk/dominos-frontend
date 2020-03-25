0198bdca9217b131092c8507c35459ae7a72da60
yatsiuk-oa <yatsiuk.oa@gbksoft.com>
2020-03-25 19:01:14 +0200

    fix server js

--- server.js
+++ server.js
@@ -6,9 +6,11 @@ const app = express();
 app.use(express.static(__dirname + '/dist/dominos-app'));
 
 // Send all requests to index.html
-app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname + '/dist/dominos-app/index.html'));
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
+console.log(path.join(__dirname))
 
 // default Heroku PORT
 app.listen(process.env.PORT || 3000);
+console.log("Server was started on localhost:3000")
