bcaaa007f591859ca9d9dc4853bd70b4b04b6c54
OleksandrYatsiuk07 <oleksandr.yatsiuk@gmail.com>
2020-07-26 21:31:17 +0300

    add success verify logic

--- src/app/auth/confirm/confirm.component.ts
+++ src/app/auth/confirm/confirm.component.ts
@@ -5,30 +5,36 @@ import { NotificationService } from 'src/app/core/services/notification.service'
 import { Route } from '@angular/compiler/src/core';
 
 @Component({
-  selector: 'app-confirm',
-  templateUrl: './confirm.component.html',
-  styleUrls: ['./confirm.component.scss']
+	selector: 'app-confirm',
+	templateUrl: './confirm.component.html',
+	styleUrls: [ './confirm.component.scss' ]
 })
 export class ConfirmComponent implements OnInit {
-  hash: any;
-  constructor(private route: ActivatedRoute,
-    private router:Router,
-    private http: UserDataService,
-    private notification: NotificationService) {
-    this.hash = this.route.snapshot.data.hash;
-  }
+	hash: any;
+	public progress = 'Verifying email...';
+	constructor(
+		private route: ActivatedRoute,
+		private router: Router,
+		private http: UserDataService,
+		private notification: NotificationService
+	) {
+		this.hash = this.route.snapshot.data.hash;
+	}
 
-  ngOnInit(): void {
-      this.http.confirm(this.hash).subscribe(({ code }) => {
-      this.notification.open({
-        data: "Email was confirmed succesfully. You can log in."
-      });
-    }, (e) => {
-      this.notification.open({
-        data: { message: e.result, status: false }
-      });
-      this.router.navigate(['/'])
-    })
-  }
-
+	ngOnInit(): void {
+		this.http.confirm(this.hash).subscribe(
+			({ code }) => {
+				this.progress = 'Verifying completed!';
+				this.notification.open({
+					data: 'Email was confirmed successfully. You can sign in to the service.'
+				});
+			},
+			(e) => {
+				this.notification.open({
+					data: { message: e.result, status: false }
+				});
+				this.router.navigate([ '/' ]);
+			}
+		);
+	}
 }
