3f1e887ab6e8f48cd3a58f7af1f8938c5efebda4
OleksandrYatsiuk07 <oleksandr.yatsiuk@gmail.com>
2020-09-07 08:53:29 +0300

    change api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  // serverUrl: 'https://dominos-backend.herokuapp.com/api/v1/',
-  serverUrl: 'http://localhost:5000/api/v1/',
+  serverUrl: 'https://dominos-backend.herokuapp.com/api/v1/',
+  // serverUrl: 'http://localhost:5000/api/v1/',
   googleApiKey:'AIzaSyAVpzeQ5eaI5OI1bUdT1mmAL0XxO3nGBLo',
 };
