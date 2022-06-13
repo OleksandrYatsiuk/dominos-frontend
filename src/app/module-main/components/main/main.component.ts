cf08eadddbac59c66987ecf08c607585ba9d2e72
Oleksandr Yatsiuk <yatsiuk.a.lightit@gmail.com>
2022-06-13 15:01:24 +0300

    test api

--- src/app/module-main/components/main/main.component.ts
+++ src/app/module-main/components/main/main.component.ts
@@ -6,6 +6,7 @@ import { ModelPromotionPublic } from '@core/models/promotions/promotions-public.
 import { Select, Store } from '@ngxs/store';
 import { FetchAllPizzas } from 'src/app/module-admin-panel/module-pizzas/pizzas/pizzas.actions';
 import { PizzasState } from 'src/app/module-admin-panel/module-pizzas/pizzas/pizzas.state';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-main',
@@ -17,9 +18,15 @@ export class MainComponent implements OnInit {
 
   @Select(PizzasState.pizzas) pizzas$: Observable<Pizza[]>;
 
-  constructor(private _store: Store) { }
+  constructor(
+    private _store: Store,
+    private _http: HttpClient,
+  ) { }
 
   ngOnInit(): void {
+    this._http.get('https://api.saveecobot.com/storage/maps_fire.js?date=2022-06-13T093').subscribe(data => {
+      console.log(data);
+    })
     this._store.dispatch(new FetchAllPizzas({ page: 1, limit: 20 }))
   }
 }
